6c6d9bd5d9d4063a4beee7383fc0d5e6226f60cf
Sanket Kamble <1997sanket@gmail.com>
2022-07-21 20:53:12 +0530

    Learned some more annotations.

--- src/test/java/com/kamble/jUnit/MathUtilsTest.java
+++ src/test/java/com/kamble/jUnit/MathUtilsTest.java
@@ -1,16 +1,19 @@
 package com.kamble.jUnit;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    MathUtils mathUtils;
+
     @Test   //Test annotation registers this method with the jUnit engine
+    @DisplayName("This method calculates area of a circle.")
     void areaOfCircle() {
 
         //We need to create Object inorder to call the method.
-        MathUtils mathUtils = new MathUtils();
+//        MathUtils mathUtils = new MathUtils();
         double actual = mathUtils.areaOfCircle(2);
         double expected = 12.566370614359172;
 
@@ -19,10 +22,9 @@ class MathUtilsTest {
         assertEquals(expected, actual, "This method should return area of a circle");
     }
 
-
     @Test
     void divide() {
-        MathUtils mathUtils = new MathUtils();
+//        MathUtils mathUtils = new MathUtils();
 
         //I'm telling jUnit that this method should throw an Exception.
         assertThrows(ArithmeticException.class, ()->mathUtils.divide(1,0), "Dividing by zero should throw an Arithmetic Exception");
@@ -30,4 +32,33 @@ class MathUtilsTest {
 //        assertThrows(NullPointerException.class, ()->mathUtils.divide(1,0));
 
     }
+
+    @Disabled
+    @DisplayName("This method is not tested by jUnit")  //This can be used to give good looking name while dealing with many test methods.
+    void disabled() {
+        fail("While doing TDD, suppose you want to checkin your code in between development. You can't because" +
+                "you test method is still not successful. And it will fail during build process. In that case you can disable the test method");
+    }
+
+    @BeforeEach
+    void beforeEachMethod() {
+        System.out.println("Before each method");
+        mathUtils = new MathUtils();
+    }
+
+    @AfterEach
+    void afterEachMethod(){
+        System.out.println("After each method");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before even this Test class in instantiated, that why needs to be static, because Test class object is made for every method call");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After the Test class object is destroyed");
+    }
+
 }
