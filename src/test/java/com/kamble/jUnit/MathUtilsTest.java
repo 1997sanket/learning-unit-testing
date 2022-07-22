51912982548de92c95ba39ccc78bffcdc032c22b
Sanket Kamble <1997sanket@gmail.com>
2022-07-22 12:59:21 +0530

    Learned some more annotations and done with basics of JUnit 5.

--- src/test/java/com/kamble/jUnit/MathUtilsTest.java
+++ src/test/java/com/kamble/jUnit/MathUtilsTest.java
@@ -8,9 +8,27 @@ class MathUtilsTest {
 
     MathUtils mathUtils;
 
+
+    //We can use a nested class if we want to test various conditions on a method. So that we logically group them, and have cleaner structure.
+    @Nested
+    class Add {
+
+        @Test
+        @DisplayName("Testing to add to two positive integers")
+        void testAddPositiveNumbers() {
+            assertEquals(2, mathUtils.add(1,1));
+        }
+
+        @Test
+        @DisplayName("Testing to add to two negative integers")
+        void testAddNegativeNumbers() {
+            assertEquals(-2, mathUtils.add(-1,-1));
+        }
+    }
+
     @Test   //Test annotation registers this method with the jUnit engine
     @DisplayName("This method calculates area of a circle.")
-    void areaOfCircle() {
+    void testAreaOfCircle() {
 
         //We need to create Object inorder to call the method.
 //        MathUtils mathUtils = new MathUtils();
@@ -19,11 +37,18 @@ class MathUtilsTest {
 
         //I'm asserting "telling" jUnit that actual and expected are equal.
         //We can add this extra message inorder for future developers to understand what this method is supposed to do exactly and make their lives easier.
-        assertEquals(expected, actual, "This method should return area of a circle");
+        //And this message is seen only when the test case is failed.
+        //This message string is processed, even before the test is failed. So that is some unnecessary processing. What if this is an expensive String ?
+        // We can avoid this by passing a Supplier lambda instead.
+
+//        assertEquals(expected, actual, "This method should return area of a circle");
+
+        assertEquals(expected, actual, () -> "This method should return area of a circle");
+
     }
 
     @Test
-    void divide() {
+    void testDivide() {
 //        MathUtils mathUtils = new MathUtils();
 
         //I'm telling jUnit that this method should throw an Exception.
@@ -31,6 +56,27 @@ class MathUtilsTest {
 
 //        assertThrows(NullPointerException.class, ()->mathUtils.divide(1,0));
 
+    }
+
+    @Test
+    void testMultiply() {
+
+        //Now suppose we want to test a method using assertEquals only, in that case instead of using Nested class we can use "assertAll" and pass all
+        //assert equals as lambdas
+
+        assertAll(
+                () -> assertEquals(2, mathUtils.multiply(2,1)),
+                () -> assertEquals(-2, mathUtils.multiply(2,-1)),
+                () -> assertEquals(0, mathUtils.multiply(2,0))
+                );
+    }
+
+    @RepeatedTest(3)    //we can repeat a test n number of times
+    void testSubtract(RepetitionInfo repetitionInfo) {  //jUnit Will inject repetition object
+
+        //We can do some processing with a certain repetition.
+        System.out.println("Current repetition: " + repetitionInfo.getCurrentRepetition());
+        assertEquals(0, 1,1);
     }
 
     @Disabled
